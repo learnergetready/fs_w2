afc010d782a95f121211fdeb8b50e4be38f10a60
Learner Getready <learnergit@hymyilee.fi>
2023-10-29 12:24:31 +0200

    2.11

--- puhelinluettelo/src/App.jsx
+++ puhelinluettelo/src/App.jsx
@@ -1,18 +1,22 @@
-import { useState } from 'react'
+import {useEffect, useState } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import peopleService from './services/persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+
+  
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('+358')
   const [filterNames, setNewFilterNames] = useState('')
+
+  useEffect(() => {
+    peopleService
+      .getAll()
+      .then( initialPeople => setPersons(initialPeople))
+  }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
